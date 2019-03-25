2d8eaed09fdb3752833d2a37f3c3e19fbe109dfd
DiogenSinopskij <mentaldistraction@gmail.com>
2019-03-25 14:29:49 +0200

    Redone ConsoleINFO

--- src/ConsoleInfo.java
+++ src/ConsoleInfo.java
@@ -1,22 +1,11 @@
 abstract class ConsoleInfo{
 	
-	public static void message()
-	{
-		System.out.println("Input arg:");
-	}
+public static void message(){System.out.println("Input arg:");}
 
-	public static void show_arg(double value)
-	{
-		System.out.println("Argument: " + value);	
-	}
+public static void show_arg(double value){System.out.println("Argument: " + value);}
 	
-	public static void show_operation(char symbol)
-	{
-		System.out.println("Operation: " + symbol);	
-	}	
+public static void show_operation(char symbol){System.out.println("Operation: " + symbol);}	
 	
-	public static void show_result(double result)
-	{
-		System.out.println("Result: " + result);
-	}
+public static void show_result(double result){System.out.println("Result: " + result);}
+
 }
